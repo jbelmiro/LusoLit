fb53cf546a060f2cc4ed0ae970083c67f507f64b
AstroSquawk <54941513+AstroSquawk@users.noreply.github.com>
2022-06-11 01:32:41 +0100

    Update to rock template
    Various UI updates

--- config.php
+++ config.php
@@ -1,12 +1,9 @@
 <?php
-//Database details
-$db_host = 'localhost';
-$db_username = 'rock_db';
-$db_password = '#&yI+%%v(QLEtb+,eR5tEDzX%)2%fFFJI%}blS@e4941K##Sz0t2u)YucQJ2c<J{';
-$db_name = 'val_test';
+
+$config = parse_ini_file('../config.ini');
 
 //Create connection and select DB
-$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+$conn = mysqli_connect($config['dbhost'], $config['username'], $config['password'], $config['dbname']);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
--- rock.php
+++ rock.php
@@ -29,29 +29,100 @@ $row = mysqli_fetch_row($result);
   <link href="css\bootstrap.css" rel="stylesheet">
   
 </head>
-<body>
+<div class="container-fluid">
+<div class="content-wrapper">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container">
+    <a class="btn btn-primary" href="https://rock.nebulatech.co.uk" role="button">Go Back</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#description" style="color: black " >Description</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#petrography" style="color: black" >Petrography</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#outcrop" style="color: black" >Outcrop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link "href="#location" style="color: black" >Location</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</nav>
+
+</nav>
+</div>
+</div>
+
+<div class="mt-4">
+<body style="padding-bottom: 50px">
 <<div class="container-fluid">       
 <div class="content-wrapper">
   <div class="container">
-    <p class="display-3">Astro's Rock Collection   <button type="button" class="btn btn-secondary" style="position:relative; left:-750px" onclick="history.back()">Go Back</button></p>
+    <p class="display-3">Astro's Rock Collection 
 
-    <p class="h3"><?php echo $row[1] ?></p>
-    <p class="lead"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Mi proin sed libero enim sed faucibus turpis in.</p>
+    <p class="h3 navanchor" style="scroll-margin-top: 2em" id="description">Description</p>
+    <p class="h5" style=" position: relative;right:-30px">Macroscopic Description</p>
+    <p class="lead alert alert-dark" role="alert""> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum orci nibh. Nam ullamcorper, nisi eu consequat porttitor, eros elit consequat elit, non egestas orci urna ac mauris. Sed vehicula non nunc efficitur mollis. Aenean in faucibus turpis, non consequat ante. Cras nec dolor turpis. Aenean velit nisi, placerat eget rhoncus eu, porttitor id purus. Aliquam massa sapien, euismod a efficitur eu, semper ac enim. Ut odio mi, posuere quis magna a, malesuada consequat ante. Integer tincidunt ultrices lectus, non gravida elit feugiat quis. Donec molestie orci est, eu vehicula purus tristique nec. Cras et consequat arcu. Morbi a rutrum sapien. Mauris eget lorem aliquet, hendrerit risus id, consequat enim</p>
    
-    <figure class="cd-image-container">
-    <img src="<?php echo $row[3] ?>" alt="Original Image">
+
+    <div class="item">
+      <figure class="cd-image-container">
+    <img src="img\SP36_005_CPL" alt="Original Image">
+    <span class="cd-image-label" data-type="original"></span>
+    </figure>
+    </div> 
+
+
+    
+<p class="h3" style="scroll-margin-top: 2em" id="petrography">Petrography</p>
+    <p class="h5" style=" position: relative;right:-30px">Description</p>
+    <p class="lead alert alert-dark" role="alert""> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum orci nibh. Nam ullamcorper, nisi eu consequat porttitor, eros elit consequat elit, non egestas orci urna ac mauris. Sed vehicula non nunc efficitur mollis. Aenean in faucibus turpis, non consequat ante. Cras nec dolor turpis. Aenean velit nisi, placerat eget rhoncus eu, porttitor id purus. Aliquam massa sapien, euismod a efficitur eu, semper ac enim. Ut odio mi, posuere quis magna a, malesuada consequat ante. Integer tincidunt ultrices lectus, non gravida elit feugiat quis. Donec molestie orci est, eu vehicula purus tristique nec. Cras et consequat arcu. Morbi a rutrum sapien. Mauris eget lorem aliquet, hendrerit risus id, consequat enim</p>
+  
+<div class="item">
+      <figure class="cd-image-container">
+    <img src="img\SP36_005_CPL" alt="Original Image">
     <span class="cd-image-label" data-type="original"></span>
 
     <div class="cd-resize-img"> <!-- the resizable image on top -->
-      <img src="<?php echo $row[4] ?>" alt="Modified Image">
+      <img src="img\SP36_005_PPL" alt="Modified Image">
       <span class="cd-image-label" data-type="modified"></span>
     </div>
     <span class="cd-handle"></span>
-  </figure> <!-- cd-image-container -->
-  <div class="mt-4">
+  </figure> 
+    </div>
+  
+    <div class="mt-4">
+
+<p class="h3" style="scroll-margin-top: 2em" id="outcrop">Outcrop</p>
+    <p class="h5" style=" position: relative;right:-30px">Description</p>
+    <p class="lead alert alert-dark" role="alert""> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum orci nibh. Nam ullamcorper, nisi eu consequat porttitor, eros elit consequat elit, non egestas orci urna ac mauris. Sed vehicula non nunc efficitur mollis. Aenean in faucibus turpis, non consequat ante. Cras nec dolor turpis. Aenean velit nisi, placerat eget rhoncus eu, porttitor id purus. Aliquam massa sapien, euismod a efficitur eu, semper ac enim. Ut odio mi, posuere quis magna a, malesuada consequat ante. Integer tincidunt ultrices lectus, non gravida elit feugiat quis. Donec molestie orci est, eu vehicula purus tristique nec. Cras et consequat arcu. Morbi a rutrum sapien. Mauris eget lorem aliquet, hendrerit risus id, consequat enim</p>
+
+<div class="item">
+      <figure class="cd-image-container">
+    <img src="img\SP36_005_CPL" alt="Original Image">
+    <span class="cd-image-label" data-type="original"></span>
+    </figure>
+    </div> 
+
   <p class="lead alert alert-dark" role="alert"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum orci nibh. Nam ullamcorper, nisi eu consequat porttitor, eros elit consequat elit, non egestas orci urna ac mauris. Sed vehicula non nunc efficitur mollis. Aenean in faucibus turpis, non consequat ante. Cras nec dolor turpis. Aenean velit nisi, placerat eget rhoncus eu, porttitor id purus. Aliquam massa sapien, euismod a efficitur eu, semper ac enim. Ut odio mi, posuere quis magna a, malesuada consequat ante. Integer tincidunt ultrices lectus, non gravida elit feugiat quis. Donec molestie orci est, eu vehicula purus tristique nec. Cras et consequat arcu. Morbi a rutrum sapien. Mauris eget lorem aliquet, hendrerit risus id, consequat enim.</p>
+ 
+<p class="h3" id="location">Location</p>
+
+<div class="mapouter"><div class="gmap_canvas"><iframe width="900" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=bishops%20stortford&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br><style>.mapouter{position:relative; right: -380;text-align:right;height:500px;width:900px;}</style><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>
   </div>
 </div>
+  </div>
+
+<div class="mt-8">
+
+    
+
+  
 
 </div>
 </div>
@@ -61,4 +132,21 @@ $row = mysqli_fetch_row($result);
 <script src="js/jquery-2.1.1.js"></script>
 <script src="js/jquery.mobile.custom.min.js"></script> <!-- Resource jQuery -->
 <script src="js/main.js"></script> <!-- Resource jQuery -->
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="text-muted">© 2022 Astro Website Design </span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+    </ul>
+  </footer>
+</div>
 </html>
