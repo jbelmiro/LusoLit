ad997abe971d6f6cff52741376b8255735dd9f74
AstroSquawk <54941513+AstroSquawk@users.noreply.github.com>
2022-10-01 19:43:20 +0100

    Search Update
    Search queries updated to match new database

--- searchresultteach.php
+++ searchresultteach.php
@@ -7,7 +7,7 @@ $return = '';
 if(isset($_POST["query"]))
 {
 	$search = mysqli_real_escape_string($conn, $_POST["query"]);
-	$query = "SELECT DISTINCT geosamples.name, geosamples.location, geosamples.id, geoprovenance.*, thinsection_id FROM geosamples INNER JOIN storage ON geosamples.storage_id = storage.storage_id INNER JOIN thinsection ON geosamples.thinsection_id = thinsection.thin_id INNER JOIN outcrop on geosamples.outcrop_id = outcrop.id INNER JOIN geoprovenance on geoprovenance.geosamples_id = geosamples.id
+	$query = "SELECT DISTINCT geosamples.name, geosamples.longitude, geosamples.latitude, geosamples.location, geosamples.id, geoprovenance.*, thinsection_id FROM geosamples INNER JOIN storage ON geosamples.storage_id = storage.storage_id INNER JOIN thinsection ON geosamples.thinsection_id = thinsection.thin_id INNER JOIN outcrop on geosamples.outcrop_id = outcrop.id INNER JOIN geoprovenance on geoprovenance.geosamples_id = geosamples.id
 	WHERE geosamples.name  LIKE '%".$search."%' 
 	OR location LIKE '%".$search."%' 
 	OR state LIKE '%".$search."%'
@@ -23,13 +23,18 @@ if(mysqli_num_rows($result) > 0)
 	
 	$return .='
 	<div class="table-responsive">
-	<table class="table table-stripe">
+	<table class="table table-secondary table-stripe">
 	<thead>
 	<tr>
 		<th scope="col">ID</th>
-		<th scope="col">Location</th>
-		<th scope="col">Outcrop State</th>
-		<th scope="col">Thin Section</th>
+		<th scope="col">Site</th>
+		<th scope="col">Area</th>
+		<th scope="col">Layer</th>
+		<th scope="col">Square</th>
+		<th scope="col">Latitude</th>
+		<th scope="col">Longitude</th>
+		<th scope="col">Altitude</th>
+		
 	</thead>
 	</tr>';
 	while($row1 = mysqli_fetch_array($result))
---
+++ val_teacher.php
@@ -0,0 +1,56 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+include("config.php");
+?>
+<html lang="">
+<head>
+    <title>MugeData</title>
+    <script src="js\bootstrap.js"></script>
+    <script src="js\jquery-3.6.0.js"></script>
+    <link href="css\bootstrap.css" rel="stylesheet">
+    <script>
+        $(document).ready(function(){
+            load_data();
+            function load_data(query)
+            {
+                $.ajax({
+                    url:"searchresultteach.php",
+                    method:"POST",
+                    data:{query:query},
+                    success:function(data)
+                    {
+                        $('#result').html(data);
+                    }
+                });
+            }
+            $('#search').keyup(function(){
+                var search = $(this).val();
+                if(search != '')
+                {
+                    load_data(search);
+                }
+                else
+                {
+                    load_data();
+                }
+            });
+        });
+    </script>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="content-wrapper">
+        <div class="container">
+            <p class="display-1">MugeData</p>
+            <div class="row margin-top:5px">
+                <div class="col-sm-12 ">
+                    <label for="search"></label><input type="text" name="search" id="search" placeholder="Search" class="form-control" />
+
+                    <div class='mt-2' id="result"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
