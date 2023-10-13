b693f5e5476bac5f3134c941a442bb94b5a12eb0
AstroSquawk <54941513+AstroSquawk@users.noreply.github.com>
2023-10-13 15:27:38 +0100

    Tweaks

--- acknowledgement.php
+++ acknowledgement.php
@@ -23,11 +23,13 @@
                 <h1 class="display-4 fw-bold">Acknowledgments</h1>
                 <div class="col-lg-6 mx-auto">
                     </br>
-                    <p class="lead mb-4 border-bottom border-danger pb-2">Science is rarely made by one single person; LusoLit is the result of the work of several researchers. For this, we would like to thank a few people and institutions:</br>
+                    <p class="mb-4 border-bottom border-danger pb-2 text-start">Science is rarely made by one single person; LusoLit is the result of the work of several researchers. For this, we would like to thank a few people and institutions:
+                        <br>
+                        <br>
                        Dr. Telmo Pereira and his team, who initially created the reference collection and strongly boosted the study of raw materials at the University of the Algarve. We thank them for their research and for creating a strong baseline, from which LusoLit can continue to grow. Jack Acres for his exceptional programming work which allowed the online lithotheque to come to life. Finally, ICArEHB (and the University of the Algarve) and FCT, which have been instrumental in hosting and funding the lithotheque, both directly and through the funding of research projects.
                     </p>
-                    <p class="lead mb-4">A ciência raramente é feita por uma única pessoa; a LusoLit é o resultado do trabalho de vários investigadores. Por isso, gostaríamos de agradecer a algumas pessoas e instituições:</br>
-                        Ao Dr. Telmo Pereira e à sua equipa, que inicialmente criaram a coleção de referência e impulsionaram fortemente o estudo de matérias-primas na Universidade do Algarve. Agradecemos-lhes pela sua pesquisa e por terem criado uma base sólida, a partir da qual a LusoLit pode continuar a crescer. Ao Jack Acres pelo seu trabalho de programação excepcional, que permitiu que a litoteca online ganhasse vida. Por fim, ao ICArEHB (e à Universidade do Algarve) e à FCT, que desempenharam um papel fundamental no acolhimento e financiamento da litoteca, tanto diretamente como através do financiamento de projetos de pesquisa.
+                    <p class="mb-4 text-start">A ciência raramente é feita por uma única pessoa; a LusoLit é o resultado do trabalho de vários investigadores. Por isso, gostaríamos de agradecer a algumas pessoas e instituições:</br>
+                        <br>Ao Dr. Telmo Pereira e à sua equipa, que inicialmente criaram a coleção de referência e impulsionaram fortemente o estudo de matérias-primas na Universidade do Algarve. Agradecemos-lhes pela sua pesquisa e por terem criado uma base sólida, a partir da qual a LusoLit pode continuar a crescer. Ao Jack Acres pelo seu trabalho de programação excepcional, que permitiu que a litoteca online ganhasse vida. Por fim, ao ICArEHB (e à Universidade do Algarve) e à FCT, que desempenharam um papel fundamental no acolhimento e financiamento da litoteca, tanto diretamente como através do financiamento de projetos de pesquisa.
                     </p>
                 </div>
 
--- footer.php
+++ footer.php
@@ -2,18 +2,18 @@
 echo '<footer class="mt-auto">
     <div class="container">
         <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <a href="https://www.icarehb.com/"> <img src="img/ICArEHB.png" href="https://www.icarehb.com/" class="img-fluid" alt="Responsive image"> </a>
+            <div class="row align-items-center justify-content-center">
+                <div class="col-sm ">
+                    <a href="https://www.icarehb.com/"> <img src="img/ICArEHB.png" href="https://www.icarehb.com/" class="img-fluid " style="height: auto; max-width:100%;" alt="Responsive image"> </a>
                 </div>
                 <div class="col-sm">
-                    <a href="https://revistas.rcaap.pt/finisterra/index"><img src="img/Finisterra.jpg" class="img-fluid" alt="Responsive image"></a>
+                    <a href="https://revistas.rcaap.pt/finisterra/index"><img src="img/Finisterra.jpg" class="img-fluid" style="height: auto; max-width:100%;" alt="Responsive image"></a>
                 </div>
                 <div class="col-sm">
-                    <a href="https://www.fct.pt/en/"><img src="img/2022_FCT_Logo_A_horizontal_preto.png" class="img-fluid" alt="Responsive image"></a>
+                    <a href="https://www.fct.pt/en/"><img src="img/2022_FCT_Logo_A_horizontal_preto.png" class="img-fluid" style="height: auto; max-width:100%;" alt="Responsive image"></a>
                 </div>
                 <div class="col-sm">
-                    <a href="https://www.ualg.pt/"><img src="img/UAlg_preto.jpg" class="img-fluid" alt="Responsive image"></a>
+                    <a href="https://www.ualg.pt/"><img src="img/UAlg_preto.jpg" class="img-fluid" style="height: auto; max-width:100%;" alt="Responsive image"></a>
                
                 </div>
             </div>
