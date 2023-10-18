b919a6f1535651d103773941b030255bf5e4caa1
AstroSquawk <54941513+AstroSquawk@users.noreply.github.com>
2023-10-18 17:51:06 +0100

    Index change

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 
 </head>
 <body class="">
+<!--
 <div class="px-4 pt-3 my-5 text-center ">
 
     <div class="" style="max-height: 35vh;">
@@ -31,7 +32,29 @@
 
 </div>
 </body>
+-->
+<div class="px-4 pt-3 my-5 text-right ">
+    <div class="" style="max-height: 70vh;">
+        <div class="container px-5">
+            <div class="row">
+            <div class="col-5"><img src="img/Welcome_to_LusoLit_v2.png" class="img-fluid  mb-4" alt="Example image" width="900" height="2747" loading="lazy"></div>
+            <div class="col-7">
+                <div class=" mx-auto border-bottom border-danger pb-2 mb-4 text-start">
+                    <p class="mb-4 text-start">LusoLit is a lithotheque and reference collection of knappable raw materials from Portugal, with a focus on chert from the Algarve region and Central Portugal. Located at ICArEHB, in the University of the Algarve, the goal of the lithotheque is to host and study geological samples of interest for raw material archaeological research. Created in 2016 by Dr. Telmo Pereira, the lithotheque currently hosts more than 100 samples of chert, quartz, and quartzite from different locations and outcrops, recently expanded by an ongoing research project led by Joana Belmiro (PhD student). This online database stands as an effort to make the lithotheque accessible to researchers and the public, allowing its consultation free and effortless, anywhere in the world.</p>
+                </div>
+                <div class="mx-auto mb-4 text-start">
+                    <p class="mb-4">LusoLit é uma litoteca e coleção de referência de matérias-primas de Portugal, com um foco em sílex da região do Algarve e do Centro de Portugal. Localizada no ICArEHB, na Universidade do Algarve, o objetivo da litoteca é armazenar e estudar amostras geológicas de interesse para o estudo arqueológico de matérias-primas. Criada em 2016 pelo Dr. Telmo Pereira, a litoteca atualmente abriga mais de 100 amostras de sílex, quartzo e quartzito de diferentes locais e afloramentos, recentemente ampliada por um projeto de pesquisa liderado por Joana Belmiro (estudante de doutoramento). Esta base de dados online representa um esforço para tornar a litoteca acessível a investigadores e ao público, permitindo a sua consulta gratuita e sem esforço, em qualquer lugar do mundo.</p>
+                </div>
 
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+</body>
 
 <?php include('footer.php')?>
 </html>
