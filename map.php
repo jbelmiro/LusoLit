d12acc89baab260bb8575906408449a58651355c
AstroSquawk <54941513+AstroSquawk@users.noreply.github.com>
2023-10-20 14:13:20 +0100

    Update map.php

--- map.php
+++ map.php
@@ -17,7 +17,7 @@
         <div class="px-4 pt-5 my-5 text-center align-content-center">
             <h1>LusoLit recovered samples</h1>
 
-          <div class="col-lg-6 mx-auto b">
+          <div class="col-lg-8 mx-auto b">
 
             <p class="lead mb-4">
             </p>
